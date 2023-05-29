0f3ed5b1505e6d06597bd1494c464b1f6244f0a2
zhihong <joker_lizhih@163.com>
2023-05-29 10:00:24 +0800

    feat: add jdtls lsp

--- lua/custom/plugins/lspconfig.lua
+++ lua/custom/plugins/lspconfig.lua
@@ -2,7 +2,7 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "rust_analyzer", "pyright" }
+local servers = { "rust_analyzer", "pyright", "jdtls" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
