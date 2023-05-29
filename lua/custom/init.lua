02a69d8c6a373c5dc34f56b26e2e28030bb32f95
zhihong <joker_lizhih@163.com>
2023-05-29 09:58:35 +0800

    change: change tab size

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -3,3 +3,7 @@ vim.g.copilot_no_tab_map = true
 vim.g.copilot_assume_mapped = true
 vim.g.copilot_tab_fallback = ""
 vim.g.codeium_no_map_tab = false
+
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
