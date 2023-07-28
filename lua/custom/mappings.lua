1fdb03f73bb267aec397862e6b4fa7a6892ba69e
zhihong <joker_lizhih@163.com>
2023-07-28 16:13:30 +0800

    Add vim-tmux-navigator plugin

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -10,6 +10,15 @@ M.disabled = {
   n = {}
 }
 
+M.general = {
+  n = {
+    ["<C-h>"] = { "<cmd> TmuxNavigateLeft<CR>", "Window left" },
+    ["<C-l>"] = { "<cmd> TmuxNavigateRight<CR>", "Window right" },
+    ["<C-j>"] = { "<cmd> TmuxNavigateDown<CR>", "Window down" },
+    ["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>", "Window up" },
+  }
+}
+
 M.todocomments = {
   n = {
     ["<leader>td"] = { "<cmd> TodoTrouble <CR>", "show todo in trouble" },
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,6 +1,11 @@
 return {
 
   {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+  },
+
+  {
       "neovim/nvim-lspconfig",
       config = function()
         require "plugins.configs.lspconfig"
